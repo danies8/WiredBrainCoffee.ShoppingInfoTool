0b9fc8f1b5710ad9aacb2127cfdbfa1e5fd032f5
Dani Shitrit <yael_b4@yahoo.com>
2022-01-18 15:51:32 +0200

    Add project

---
+++ WiredBrainCoffee.ShoppingInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee.ShoppingInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee Shopping Info Tool!");
+        }
+    }
+}
