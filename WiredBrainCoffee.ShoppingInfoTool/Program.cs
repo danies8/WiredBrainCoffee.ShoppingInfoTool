2f6c472f329ae479929620d42fd1b0fcfdad4cbe
Dani Shitrit <yael_b4@yahoo.com>
2022-01-18 22:30:20 +0200

    from branch 8

--- WiredBrainCoffee.ShoppingInfoTool/Program.cs
+++ WiredBrainCoffee.ShoppingInfoTool/Program.cs
@@ -7,38 +7,10 @@ namespace WiredBrainCoffee.ShoppingInfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            // branches 8
 
-            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
-              "write 'quit' to exit application");
 
-            var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
-            while (true)
-            {
-                var line = Console.ReadLine();
-
-                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    break;
-                }
-
-                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
-
-                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("> Available coffee shop commands:");
-                    foreach (var coffeeShop in coffeeShops)
-                    {
-                        Console.WriteLine($"> " + coffeeShop.Location);
-                    }
-                }
-            }
-            //branch 6
-
-            // branch 1
-            // branch 2 master
-            // master
 
         }
     }
